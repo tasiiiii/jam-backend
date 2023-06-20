e7556a480439a363bed8b4ebbeeabbd0fe25161d
Artem Tyutnev <tasiiiii.net@gmail.com>
2023-06-21 00:09:29 +0300

    Добавлен пакет swagger

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -6,6 +6,10 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as IlluminateController;
 
+/**
+ * @OA\Info(title="Jam", version="0.0.1")
+ * @OA\PathItem(path="/")
+ */
 class BaseController extends IlluminateController
 {
     use AuthorizesRequests, ValidatesRequests;
