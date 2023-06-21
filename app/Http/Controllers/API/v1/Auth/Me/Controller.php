c9322798462d6e458b897bc0985791108834f34a
Artem Tyutnev <tasiiiii.net@gmail.com>
2023-06-21 17:10:00 +0300

    Обновлена документация

--- app/Http/Controllers/API/v1/Auth/Me/Controller.php
+++ app/Http/Controllers/API/v1/Auth/Me/Controller.php
@@ -7,6 +7,48 @@ use App\Jam\Auth\Service\UserProviderInterface;
 use App\UI\Response\JsonResponseFactory;
 use Illuminate\Http\JsonResponse;
 
+/**
+ * @OA\Post(
+ *     path="/api/v1/auth/me",
+ *     summary="Get current user",
+ *     @OA\Response(
+ *         response=200,
+ *         description="Success",
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                      property="message",
+ *                      type="string"
+ *                 ),
+ *                 @OA\Property(
+ *                      property="data",
+ *                      type="array",
+ *                      @OA\Items(
+ *                          @OA\Property(property="first_name", type="string"),
+ *                          @OA\Property(property="middle_name", type="string"),
+ *                          @OA\Property(property="last_name", type="string"),
+ *                          @OA\Property(property="email", type="string")
+ *                      )
+ *                 )
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=403,
+ *         description="Error",
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                      property="message",
+ *                      type="string"
+ *                 )
+ *             )
+ *         )
+ *     )
+ * )
+ */
 class Controller extends BaseController
 {
     public function __construct(
